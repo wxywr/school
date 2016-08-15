333397836d743de50f771529976deadd2cbb6b86
wxywr <1261046195@qq.com>
2016-08-15 23:25:18 +0800

    update

--- js/school.js
+++ js/school.js
@@ -10,3 +10,15 @@ $(document).ready(function(){
 		$("h2").html("查看更多");
 	});
 });
+
+
+$(document).ready(function(){
+	$(".button").click(function(){
+		$("#layer").show();
+		$(".wish").show();
+	});
+	$("#close").click(function(){
+		$("#layer").hide();
+		$(".wish").hide();
+	});
+});
