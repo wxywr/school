12b7e4b9afb7a508eb636dd0e3bde8a0054cfa6e
wxywr <1261046195@qq.com>
2016-08-15 22:00:03 +0800

    开学心愿

---
+++ js/school.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$("h2").click(function(){
+		$("span").show();
+		$("h2").css("color","red");
+		$("h2").html("收起内容");
+	});
+	$("h2").dblclick(function(){
+		$("span").hide();
+		$("h2").css("color","blue");
+		$("h2").html("查看更多");
+	});
+});
